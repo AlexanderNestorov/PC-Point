805a1b8c44559aae1bf97d45951e9c09d0ab12eb
Alexander Nestorov <jak.racer@gmail.com>
2021-11-03 11:37:30 +0200

    Initial Commit.

---
+++ src/main/java/com/example/pcpoint/PcPointApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcPointApplication.class, args);
+    }
+
+}
