3d946e1b5bedb72f0dae707f237e09c3ca3d6f8a
Alexander Nestorov <jak.racer@gmail.com>
2021-11-07 16:07:45 +0200

    Trying JSON Web Token implementation.

--- src/main/java/com/example/pcpoint/config/ApplicationConfig.java
+++ src/main/java/com/example/pcpoint/config/ApplicationConfig.java
@@ -3,16 +3,11 @@ package com.example.pcpoint.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
 
 @Configuration
 public class ApplicationConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new Pbkdf2PasswordEncoder();
-    }
 
     @Bean
     public ModelMapper modelMapper() {
---
+++ src/main/java/com/example/pcpoint/security/user/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.pcpoint.security.user;
+
+import com.example.pcpoint.model.entity.user.UserEntity;
+import com.example.pcpoint.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found!"));
+
+        return UserDetailsImpl.build(user);
+    }
+}
