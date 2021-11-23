26099e35d9f9715f42c94fed6c6493e7bc307545
Alexander Nestorov <jak.racer@gmail.com>
2021-11-23 12:30:46 +0200

    Email interceptor To-Do

--- src/main/java/com/example/pcpoint/config/WebConfig.java
+++ src/main/java/com/example/pcpoint/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.pcpoint.config;
 
+import com.example.pcpoint.controller.email.EmailInterceptor;
 import com.example.pcpoint.controller.stats.StatsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,13 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final StatsInterceptor statsInterceptor;
+    private final EmailInterceptor emailInterceptor;
 
-    public WebConfig(StatsInterceptor statsInterceptor) {
+    public WebConfig(StatsInterceptor statsInterceptor, EmailInterceptor emailInterceptor) {
         this.statsInterceptor = statsInterceptor;
+        this.emailInterceptor = emailInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(statsInterceptor);
+        registry.addInterceptor(emailInterceptor).addPathPatterns("/api/auth/register");
     }
 }
